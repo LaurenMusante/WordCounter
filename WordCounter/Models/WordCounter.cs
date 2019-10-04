1dea80d9f14a6beb43b87bd0e3b1bab2bc56e4bf
Lauren <Lauren.Musante@gmail.com>
2019-10-04 13:14:25 -0700

    Begin to enter code to have my method ignore punctuation marks.

--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -7,7 +7,9 @@ namespace Counter
     {
         public int WordCounter(string word, string sentence)
         {
+            //string NewString = MyString.TrimEnd(MyChar);
             string [] words = sentence.Split(" ");
+            string noPeriods = words[words.Length + 1].TrimEnd(string .);
             int wordCount = 0;
             for(int i=0; i < words.Length; i++)
             {
